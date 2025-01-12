e1753b974fb3d451a13b8477fd7076ab633aa76a
Jaegerist2 <166934589+Jaegerist2@users.noreply.github.com>
2025-01-12 14:25:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2349014805359"
 
 
 
@@ -19,7 +19,7 @@ global.anticall = false
 global.autoReadChat = false
 global.alwaysonline = true
 global.autoswview = false
-global.public = true
+global.public = false
 global.autoreact = false
 global.antidelete = false
 global.autotyping = false
